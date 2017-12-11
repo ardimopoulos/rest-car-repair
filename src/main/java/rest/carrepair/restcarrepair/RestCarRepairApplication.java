c5d9b1fe268d9b4f3bfae13b213c8f795f729c9e
Akis <ardimopoulos@gmail.com>
2017-12-12 00:31:56 +0200

    test commit

---
+++ src/main/java/rest/carrepair/restcarrepair/RestCarRepairApplication.java
@@ -0,0 +1,12 @@
+package rest.carrepair.restcarrepair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCarRepairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCarRepairApplication.class, args);
+	}
+}
