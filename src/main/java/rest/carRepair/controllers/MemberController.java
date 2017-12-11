ba36233aa545896a1840a1f49c7619886803052c
Akis <ardimopoulos@gmail.com>
2017-12-12 01:44:04 +0200

    member controller methods

--- src/main/java/rest/carRepair/controllers/MemberController.java
+++ src/main/java/rest/carRepair/controllers/MemberController.java
@@ -1,4 +1,24 @@
 package rest.carRepair.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import rest.carRepair.domain.Member;
+
+import java.util.List;
+
+@RestController
 public class MemberController {
+
+    @GetMapping("/members")
+    public List<Member> getAllusers(){
+
+        return null;
+    }
+
+    @GetMapping("/members/{id}")
+    public Member getMember(@PathVariable String id){
+
+        return null;
+    }
 }
