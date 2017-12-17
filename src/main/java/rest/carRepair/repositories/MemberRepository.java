fdf64613412fe373d0c3cad62b044a8231b4379f
Akis <ardimopoulos@gmail.com>
2017-12-17 22:30:56 +0200

    updated member

--- src/main/java/rest/carRepair/repositories/MemberRepository.java
+++ src/main/java/rest/carRepair/repositories/MemberRepository.java
@@ -1,10 +1,12 @@
 package rest.carRepair.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import rest.carRepair.domain.Member;
 
 import java.util.List;
 
+@Repository
 public interface MemberRepository extends CrudRepository<Member, Long> {
 
     List<Member> findAll();
