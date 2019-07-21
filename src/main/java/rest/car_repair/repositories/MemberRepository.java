1cf13451e9f688f3af71eb1c446a14bb00ac19bf
ardimopoulos <ardimopoulos@gmail.com>
2019-07-22 00:49:56 +0300

    Update member repository.

--- src/main/java/rest/car_repair/repositories/MemberRepository.java
+++ src/main/java/rest/car_repair/repositories/MemberRepository.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Repository;
 import rest.car_repair.domain.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Long> {
     List<Member> findAll();
+
+    Optional<Member> findByEmail(String email);
 }
